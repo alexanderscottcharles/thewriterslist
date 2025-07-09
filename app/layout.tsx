897370b07524df2870e519dc9cc062eb72d3551b
alexanderscottcharles <alexanderscottcharles@gmail.com>
2025-07-08 22:22:37 -0700

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Playfair_Display } from 'next/font/google'
+import type { Metadata } from 'next'
 
 const playfair = Playfair_Display({
   subsets: ['latin'],
@@ -8,10 +9,26 @@ const playfair = Playfair_Display({
  
 })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'The Writers List',
   description: 'The Only Website by Writers, for Writers',
-};
+    openGraph: {
+    title: 'The Writers List',
+    description: 'A peer network and submission platform for writers.',
+    url: 'https://thewriterslist.com',
+    siteName: 'The Writers List',
+    images: [
+      {
+        url: 'https://thewriterslist.com/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'The Writers List Preview',
+      },
+    ],
+    type: 'website',
+  }
+}
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
