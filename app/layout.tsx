e4d0f886b534bb906c8325a407a7a7a5f09c649f
alexanderscottcharles <alexanderscottcharles@gmail.com>
2025-07-08 22:26:07 -0700

    public??

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     siteName: 'The Writers List',
     images: [
       {
-        url: 'https://thewriterslist.com/opengraph-image.png',
+        url: 'https://thewriterslist.com/public/opengraph-image.png',
         width: 1200,
         height: 630,
         alt: 'The Writers List Preview',
