a4390e0395b3b1edd28f6da3993725bcfff7ffea
alexanderscottcharles <alexanderscottcharles@gmail.com>
2025-07-10 11:16:20 -0700

    lytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${playfair.variable}`}>
+    <html lang="en" className={`${playfair.variable}`}> <Analytics/>
       <body>{children}</body>
     </html>
   );
